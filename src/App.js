9b91cc2f2dd810d049b90d88a23ff40787830a17
babak abbasi <Babak.Abbasi.develop@gmail.com>
2024-10-24 13:49:08 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import WalletOutlined from '@mui/icons-material/WalletOutlined';
+import axios from 'axios';
+
+import '@fontsource/roboto/300.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-title">
+          Wallet
+          <WalletOutlined color="success" fontSize="larger"></WalletOutlined>
+          <Button variant="contained" onClick={() => {alert('get');}}>Contained</Button>
+        </div>
       </header>
     </div>
   );
+}
+
+function get(){
+  axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 }
 
 export default App;
